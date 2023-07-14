239f0567e0800d2ac52da7327d686e579047713a
namiqmamedov <namigmm@code.edu.az>
2023-07-14 23:28:36 +0400

    Updated extension method

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -22,10 +22,12 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy)
+        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy,string searchTerm,string brands,string types)
         {
             var query = _context.Products
             .Sort(orderBy)
+            .Search(searchTerm)
+            .Filter(brands, types)
             .AsQueryable();
 
             return await query.ToListAsync();
